24e1b740fe01cadc9e7cfd2d6a7322f05a5b01f2
Marck-vsv <marck.vinisv@outlook.com>
2023-11-02 15:17:23 -0300

    Criação de pacotes e classes a serem usados

---
+++ src/sistema/Professor.java
@@ -0,0 +1,5 @@
+package sistema;
+
+public class Professor extends Usuario {
+    
+}
---
+++ src/sistema/Usuario.java
@@ -0,0 +1,48 @@
+package sistema;
+
+public class Usuario {
+    private String nome;
+    private String cargo;
+
+    private String matricula;
+    private String cpf;
+
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
+}
